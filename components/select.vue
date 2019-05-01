d8dfa940ede624c55859a849767feafaacdcc03e
DangoSky <1341514988@qq.com>
2019-05-01 21:04:59 +0800

    将多选框抽出成组件

---
+++ components/select.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <p class="select">请选择碎片的数量</p>
+    <!-- <ul>
+      <li v-for="item in options">
+        {{item}}
+      </li>
+    </ul> -->
+  </div>
+</template>
+
+<script>
+  export default {
+    // props: ['options']
+  }
+</script>
+
+<style lang="less" scoped>
+  .select {
+    background: rgb(216, 202, 202) url('../image//arrow.png') no-repeat calc(100% - 10px) center;
+    border-radius: 10px;
+    /* color: white; */
+    /* border: 1px solid red; */
+    display: inline-block;
+    padding: 5px 40px 5px 5px;
+  }
+</style>
